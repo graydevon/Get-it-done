2fade8763329f7e48d511331aed028e131589cf4
Devon <devondgray28@gmail.com>
2022-07-04 04:28:38 -0500

    style.css

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,18 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
-getUserRepos();
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+    var formSubmitHandler = function(event) {
+      event.preventDefault();
+      console.log(event); 
+    }
+  };
